8407837aa686be87cdf75360b41639066eaaadb8
EuniceJoshi <eunicejoshiap@yantravision.com>
2026-01-22 17:25:59 +0530

    updated

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ void checkNumber(int n) {
     if (n <= 1)
         isPrime = 0;
 
-    for (i = 2; i < n/2; i++) {
+    for (i = 0; i < n; i++) {
         if (n % i == 0) {
             isPrime = 0;
             break;
@@ -13,7 +13,7 @@ void checkNumber(int n) {
     }
 
     if (isPrime)
-        printf("Prime number\n");
+        printf("%d is Prime number\n",n);
     else
-        printf("Not a prime number\n");
+        printf("%d is Not a prime number\n",n);
 }
