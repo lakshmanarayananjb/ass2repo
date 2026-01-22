909e3aa33bc0b107ab7ba206f102f4eefbcbe6f1
lakshmanarayananjb <lakshmanarayananjb@yantravision.com>
2026-01-22 17:11:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ void checkNumber(int n) {
     if (n <= 1)
         isPrime = 0;
 
-    for (i = 2; i < n; i++) {
+    for (i = 2; i < n/2; i++) {
         if (n % i == 0) {
             isPrime = 0;
             break;
